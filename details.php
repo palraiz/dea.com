701017bba39778223296271be9c57b4b660cf6ff
palraiz <palraiz.studio@gmail.com>
2021-09-21 10:32:26 +0300

    Каркас
    Подстроенный под себя каркас главной страницы и детальной информации о товаре

---
+++ details.php
@@ -0,0 +1,332 @@
+<?php
+?>
+
+<!DOCTYPE HTML>
+
+<html lang = "ru">
+
+<head>
+    <meta charset = "UTF-8">
+    <title> Deeacom </title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <style>
+        @font-face {
+            font-family: 'Web font';
+            src: url('fonts/Bebas-Neue/bebasneueregular.woff') format('woff'),
+            url('fonts/Bebas-Neue/bebasneueregular.woff2') format('woff2');
+            font-weight: normal;
+            font-style: normal;
+        }
+    </style>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/slider.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/productviewgallery.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+
+<body>
+
+<div class="header">
+    <div class="logo">
+        <!--            // Логотип-->
+        <a href="index.php"><img src="images/deeacom.png" width="200" height="80" alt=""/> </a>
+    </div>
+    <div class="h_icon">
+        <!--            // Корзина с ценником-->
+        <ul class="icon1 sub-icon1">
+            <li><a class="active-icon c1" href="#"><i>$300</i></a>
+                <ul class="sub-icon1 list">
+                    <li><h3>shopping cart empty</h3><a href=""></a></li>
+                    <li><p>if items in your wishlit are missing, <a href="contact.html">contact us</a> to view them</p></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="h_search">
+        <!--            // Поисковик-->
+        <form>
+            <input type="text" value="">
+            <input type="submit" value="">
+        </form>
+    </div>
+    <div class="clear"></div>
+    <!--        Отделение от содержимого-->
+</div>
+
+<div class="header_btm">
+    <div class="wrap">
+        <div class="header_sub">
+            <div class="h_menu">
+                <!--            Верхнее меню-->
+                <ul>
+                    <li class="active"><a href="index.php">Главная</a></li> |
+                    <li><a href="">Пункт меню</a></li> |
+                    <li><a href="">Пункт меню</a></li> |
+                    <li><a href="">Пункт меню</a></li> |
+                    <li><a href="">Пункт меню</a></li>
+                </ul>
+            </div>
+            <div class="clear"></div>
+            <!--        Отделение от содержимого-->
+        </div>
+    </div>
+</div>
+
+
+<div class="main_bg">
+    <div class="wrap">
+        <div class="main">
+            <div class="single">
+                <div class="left_content">
+                    <div class="span1_of_1">
+                        <div class="product-view">
+                            <div class="product-essential">
+                                <div class="product-img-box">
+                                    <div class="more-views" style="float:left;">
+                                        <div class="more-views-container">
+                                            <ul>
+                                                <li>
+                                                    <a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+                                                        <img src="" src_main=""  title="" alt="" /></a>
+                                                </li>
+                                                <li>
+                                                    <a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+                                                        <img src="" src_main=""  title="" alt="" /></a>
+                                                </li>
+                                                <li>
+                                                    <a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+                                                        <img src="" src_main=""  title="" alt="" /></a>
+                                                </li>
+                                                <li>
+                                                    <a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+                                                        <img src="" src_main="" title="" alt="" /></a>
+                                                </li>
+                                                <li>
+                                                    <a class="cs-fancybox-thumbs" data-fancybox-group="thumb" style="width:64px;height:85px;" href=""  title="" alt="">
+                                                        <img src="" src_main="" title="" alt="" /></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="product-image">
+                                        <a class="cs-fancybox-thumbs cloud-zoom" rel="adjustX:30,adjustY:0,position:'right',tint:'#202020',tintOpacity:0.5,smoothMove:2,showTitle:true,titleOpacity:0.5" data-fancybox-group="thumb" href="images/0001-2.jpg" title="Women Shorts" alt="Women Shorts">
+                                            <img src="images/pic1.jpg" alt="Women Shorts" title="Women Shorts" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="span1_of_1_des">
+                        <div class="desc1">
+                            <h3>Lorem Ipsum is simply dummy text </h3>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <h5>Rs. 399 <a href="#">click for offer</a></h5>
+                            <div class="available">
+                                <h4>Available Options :</h4>
+                                <ul>
+                                    <li>Color:
+                                        <select>
+                                            <option>Silver</option>
+                                            <option>Black</option>
+                                            <option>Dark Black</option>
+                                            <option>Red</option>
+                                        </select></li>
+                                    <li>Size:
+                                        <select>
+                                            <option>L</option>
+                                            <option>XL</option>
+                                            <option>S</option>
+                                            <option>M</option>
+                                        </select>
+                                    </li>
+                                    <li>Quality:
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </li>
+                                </ul>
+                                <div class="btn_form">
+                                    <form>
+                                        <input type="submit" value="add to cart" title="" />
+                                    </form>
+                                </div>
+                                <span class="span_right"><a href="#">login to save in wishlist </a></span>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="share-desc">
+                                <div class="share">
+                                    <h4>Share Product :</h4>
+                                    <ul class="share_nav">
+                                        <li><a href="#"><img src="images/facebook.png" title="facebook"></a></li>
+                                        <li><a href="#"><img src="images/twitter.png" title="Twiiter"></a></li>
+                                        <li><a href="#"><img src="images/rss.png" title="Rss"></a></li>
+                                        <li><a href="#"><img src="images/gpluse.png" title="Google+"></a></li>
+                                    </ul>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                    <section class="tabs">
+                        <input id="tab-1" type="radio" name="radio-set" class="tab-selector-1" checked="checked">
+                        <label for="tab-1" class="tab-label-1">overview</label>
+
+                        <input id="tab-2" type="radio" name="radio-set" class="tab-selector-2">
+                        <label for="tab-2" class="tab-label-2">consumer electronics</label>
+
+                        <input id="tab-3" type="radio" name="radio-set" class="tab-selector-3">
+                        <label for="tab-3" class="tab-label-3">semiconductor</label>
+
+                        <div class="clear-shadow"></div>
+
+                        <div class="content">
+                            <div class="content-1">
+                                <p class="para top"><span>LOREM IPSUM</span> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined </p>
+                                <ul>
+                                    <li>Research</li>
+                                    <li>Design and Development</li>
+                                    <li>Porting and Optimization</li>
+                                    <li>System integration</li>
+                                    <li>Verification, Validation and Testing</li>
+                                    <li>Maintenance and Support</li>
+                                </ul>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="content-2">
+                                <p class="para"><span>WELCOME </span> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections </p>
+                                <ul class="qua_nav">
+                                    <li>Multimedia Systems</li>
+                                    <li>Digital media adapters</li>
+                                    <li>Set top boxes for HDTV and IPTV Player applications on various Operating Systems and Hardware Platforms</li>
+                                </ul>
+                            </div>
+                            <div class="content-3">
+                                <p class="para top"><span>LOREM IPSUM</span> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined </p>
+                                <ul>
+                                    <li>Research</li>
+                                    <li>Design and Development</li>
+                                    <li>Porting and Optimization</li>
+                                    <li>System integration</li>
+                                    <li>Verification, Validation and Testing</li>
+                                    <li>Maintenance and Support</li>
+                                </ul>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div class="left_sidebar">
+                    <div class="sellers">
+                        <h4>Best Sellers</h4>
+                        <div class="single-nav">
+                            <ul>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">The standard chunk of Lorem Ipsum</a></li>
+                                <li><a href="#">The standard chunk of Lorem Ipsum</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">The standard chunk of Lorem Ipsum</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">The standard chunk of Lorem Ipsum</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                                <li><a href="#">Always free from repetition</a></li>
+                            </ul>
+                        </div>
+                        <div class="banner-wrap bottom_banner color_link">
+                            <a href="#" class="main_link">
+                                <figure><img src="images/delivery.png" alt=""></figure>
+                                <h5><span>Free Shipping</span><br> on orders over $99.</h5><p>This offer is valid on all our store items.</p></a>
+                        </div>
+                        <div class="brands">
+                            <h1>Brands</h1>
+                            <div class="field">
+                                <select class="select1">
+                                    <option>Please Select</option>
+                                    <option>Lorem ipsum dolor sit amet</option>
+                                    <option>Lorem ipsum dolor sit amet</option>
+                                    <option>Lorem ipsum dolor sit amet</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Футер -->
+<div class="footer_bg">
+    <div class="wrap">
+        <div class="footer">
+            <div class="grids_of_4">
+                <div class="grid1_of_4">
+                    <h4>Украшения из смолы</h4>
+                    <ul class="f_nav">
+                        <li><a href="">Вид один</a></li>
+                        <li><a href="">Вид два</a></li>
+                        <li><a href="">Вид три</a></li>
+                        <li><a href="">Вид четыре</a></li>
+                    </ul>
+                </div>
+                <div class="grid1_of_4">
+                    <h4>Вялянные фигурки</h4>
+                    <ul class="f_nav">
+                        <li><a href="">Вид один</a></li>
+                        <li><a href="">Вид два</a></li>
+                        <li><a href="">Вид три</a></li>
+                        <li><a href="">Вид четыре</a></li>
+                    </ul>
+                </div>
+                <div class="grid1_of_4">
+                    <h4>Вязанные изделия</h4>
+                    <ul class="f_nav">
+                        <li><a href="">Вид один</a></li>
+                        <li><a href="">Вид два</a></li>
+                        <li><a href="">Вид три</a></li>
+                        <li><a href="">Вид четыре</a></li>
+                    </ul>
+                </div>
+                <div class="grid1_of_4">
+                    <h4>Связь с нами</h4>
+                    <ul class="f_nav">
+                        <li><a href="">Вид один</a></li>
+                        <li><a href="">Вид два</a></li>
+                        <li><a href="">Вид три</a></li>
+                        <li><a href="">Вид четыре</a></li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Футер -->
+<div class="footer_bg1">
+    <div class="wrap">
+        <div class="footer">
+            <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+            <div class="copy">
+                <p class="link">&copy;  All rights reserved | Template by&nbsp; Palraiz Studio</p>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,224 @@
 <?php
+?>
+
+<!DOCTYPE HTML>
+<html lang = "ru">
+
+
+<head>
+    <meta charset = "UTF-8">
+    <title> Deeacom </title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <style>
+        @font-face {
+            font-family: 'Web font';
+            src: url('fonts/Bebas-Neue/bebasneueregular.woff') format('woff'),
+                 url('fonts/Bebas-Neue/bebasneueregular.woff2') format('woff2');
+            font-weight: normal;
+            font-style: normal;
+        }
+        </style>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/slider.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/productviewgallery.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+
+<body>
+
+	<div class="header">
+		<div class="logo">
+<!--            // Логотип-->
+			<a href="index.php"><img src="images/deeacom.png" width="200" height="80" alt=""/> </a>
+		</div>
+		<div class="h_icon">
+<!--            // Корзина с ценником-->
+		<ul class="icon1 sub-icon1">
+			<li><a class="active-icon c1" href="#"><i>$300</i></a>
+				<ul class="sub-icon1 list">
+					<li><h3>shopping cart empty</h3><a href=""></a></li>
+					<li><p>if items in your wishlit are missing, <a href="contact.html">contact us</a> to view them</p></li>
+				</ul>
+			</li>
+		</ul>
+		</div>
+		<div class="h_search">
+<!--            // Поисковик-->
+    		<form>
+    			<input type="text" value="">
+    			<input type="submit" value="">
+    		</form>
+		</div>
+		<div class="clear"></div>
+<!--        Отделение от содержимого-->
+	</div>
+
+<div class="header_btm">
+<div class="wrap">
+	<div class="header_sub">
+		<div class="h_menu">
+<!--            Верхнее меню-->
+			<ul>
+				<li class="active"><a href="index.php">Главная</a></li> |
+				<li><a href="">Пункт меню</a></li> |
+				<li><a href="">Пункт меню</a></li> |
+				<li><a href="">Пункт меню</a></li> |
+				<li><a href="">Пункт меню</a></li>
+			</ul>
+		</div>
+	<div class="clear"></div>
+        <!--        Отделение от содержимого-->
+</div>
+</div>
+</div>
+
+<!-- Полоса товаров -->
+    <div class="clear"></div>
+    <br/>
+    <div class="clear"></div>
+<div class="main_bg1">
+<div class="wrap">
+	<div class="main1">
+		<h2>Избранные изделия </h2>
+	</div>
+</div>
+</div>
+
+<!-- Список товаров -->
+<div class="main_bg">
+<div class="wrap">
+	<div class="main">
+		<div class="grids_of_3">
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic1.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic2.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic3.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="grids_of_3">
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic4.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic5.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="grid1_of_3">
+				<a href="details.php">
+					<img src="images/pic6.jpg" alt=""/>
+					<h3>Уникальное кольцо</h3>
+					<div class="price">
+						<h4>500₽<span>Заказать</span></h4>
+					</div>
+					<span class="b_btm"></span>
+				</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
+</div>
+
+<!-- Футер -->
+<div class="footer_bg">
+<div class="wrap">
+	<div class="footer">
+		<div class="grids_of_4">
+			<div class="grid1_of_4">
+				<h4>Украшения из смолы</h4>
+				<ul class="f_nav">
+					<li><a href="">Вид один</a></li>
+					<li><a href="">Вид два</a></li>
+					<li><a href="">Вид три</a></li>
+					<li><a href="">Вид четыре</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>Вялянные фигурки</h4>
+				<ul class="f_nav">
+                    <li><a href="">Вид один</a></li>
+                    <li><a href="">Вид два</a></li>
+                    <li><a href="">Вид три</a></li>
+                    <li><a href="">Вид четыре</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>Вязанные изделия</h4>
+				<ul class="f_nav">
+                    <li><a href="">Вид один</a></li>
+                    <li><a href="">Вид два</a></li>
+                    <li><a href="">Вид три</a></li>
+                    <li><a href="">Вид четыре</a></li>
+				</ul>
+			</div>
+			<div class="grid1_of_4">
+				<h4>Связь с нами</h4>
+				<ul class="f_nav">
+                    <li><a href="">Вид один</a></li>
+                    <li><a href="">Вид два</a></li>
+                    <li><a href="">Вид три</a></li>
+                    <li><a href="">Вид четыре</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</div>
+</div>
+
+<!-- Футер -->
+<div class="footer_bg1">
+<div class="wrap">
+	<div class="footer">
+		 <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+		<div class="copy">
+			<p class="link">&copy;  All rights reserved | Template by&nbsp; Palraiz Studio</p>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+</div>
+
+</body>
+</html>
